0879c71da89de77f66fd1863bc0901728264360f
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-11-11 18:21:25 -0600

    Finish Instructions style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,15 @@ import Instructions from "./Instructions";
 function Header() {
     const [showInstructions, setShowInstructions] = useState(false);
 
-    const toggleInstructions = () => {
-        setShowInstructions(!showInstructions);
+    const toggleInstructions = (e) => {
+        if (e.target.dataset.clickable) setShowInstructions(!showInstructions);
     };
 
     return <header className="header">
         <h1 className="header__title">Poke Memory</h1>
-        <div className="header__instructions-container">
-            <button className="header__instructions" onClick={toggleInstructions}>How to play</button>
+        <div className="header__btn-container">
+            <button className="header__btn" onClick={toggleInstructions} data-clickable>How to play</button>
+            <div className="header__underline"></div>
         </div>
         <Instructions isVisible={showInstructions} onClick={toggleInstructions} />
     </header>
--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,17 +1,20 @@
 import "../styles/Instructions.scss";
 
 function Instructions({isVisible, onClick}) {
-    return <div className="instructions" style={{display: isVisible ? "block" : "none"}}>
-        <h2 className="instructions__heading">Instructions</h2>
-        <p className="instructions__text">Each round, we'll load a set of cards with Pokemon in them.</p>
-        <p className="instructions__text">Your mission: <strong>click on all the Pokemon in the set to catch them, but without repeating Pokemon!</strong></p>
-        <p className="instructions__text">If you click the same pokemon twice during a round, you lose.</p>
-        <p className="instructions__text">If you click all the pokemon in a round without repeating, you'll get a new set with more cards.</p>
-        <p className="instructions__text">Pokemon CAN repeat between rounds. You won't lose by clicking a Pokemon you clicked in a previous round.</p>
-        <p className="instructions__text">Try to catch as many Pokemon as you can.</p>
-        <p className="instructions__text">Catch 'em all to become a Pokemon Master!</p>
-        <button className="instructions__close" onClick={onClick}>X</button>
-    </div>
+    return <>
+        <div className={`backdrop ${isVisible && "hidden"}`} onClick={onClick} data-clickable></div>
+        <div className={`instructions ${isVisible && "hidden"}`} >
+            <h2 className="instructions__heading">Instructions</h2>
+            <p className="instructions__text">Each round, we'll load a set of cards with Pokemon in them.</p>
+            <p className="instructions__text">Your mission: <strong>click on all the Pokemon in the set to catch them, but without repeating Pokemon!</strong></p>
+            <p className="instructions__text">If you click the same pokemon twice during a round, you lose.</p>
+            <p className="instructions__text">If you click all the pokemon in a round without repeating, you'll get a new set with more cards.</p>
+            <p className="instructions__text">Pokemon CAN repeat between rounds. You won't lose by clicking a Pokemon you clicked in a previous round.</p>
+            <p className="instructions__text">Try to catch as many Pokemon as you can.</p>
+            <p className="instructions__text">Catch 'em all to become a Pokemon Master!</p>
+            <button className="instructions__close" onClick={onClick} data-clickable>X</button>
+        </div>
+    </>
 }
 
 export default Instructions;
